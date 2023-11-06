590c084f425692acaef32a814f1b475730653f96
Mark H <mark-holding@hotmail.co.uk>
2023-11-06 12:51:24 +0000

    fix case errors and computer choice variable name error.

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-function getComputerChoice() {
+function computerChoice() {
     let compRandNumber = Math.floor(Math.random()*3) 
     if(compRandNumber === 0){
-        return 'Rock';
+        return 'rock';
     } else if(compRandNumber === 1){
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 };
 
-getComputerChoice()
+
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection.toLowerCase() === computerSelection){
+        console.log('Draw!')
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors'){
+        console.log('You win! Rock beats scissors.');
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
+        console.log('You lose! Rock loses to paper.');
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock'){
+        console.log('You win! Paper beats rock.');
+    } else if(playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors'){
+        console.log('You lose! Paper loses to scissors.');
+    } else if(playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock'){
+        console.log('You lose! Scissors beats rock.');
+    } else if(playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper'){
+        console.log('You win! Scissors beats paper.');
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerChoice();
+console.log(playRound(playerSelection, computerSelection))
